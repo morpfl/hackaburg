dda6abd98068887ab7718b8f0e2f8e59923f696c
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2019-05-24 14:41:44 +0200

    created main structure

---
+++ ibot-parent/ibot-backend/src/main/java/com/hackathon/ibot/IbotApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.ibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbotApplication.class, args);
+	}
+
+}
