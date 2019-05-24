95b6b2d7beab886a0c0cec7331a4ec9c1f3f0164
Jakob Neumann <mail@jakobneumann.com>
2019-05-24 15:25:09 +0200

    Switch angular project

--- ibot-frontend/src/app/app.module.ts
+++ ibot-frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
