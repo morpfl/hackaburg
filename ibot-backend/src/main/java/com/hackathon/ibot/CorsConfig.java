776dd79ac9aea7f46ee56e4a03c2a53639a5bdf4
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2019-05-25 02:51:06 +0200

    added cors configuration

---
+++ ibot-backend/src/main/java/com/hackathon/ibot/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.hackathon.ibot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+public class CorsConfig {
+	@Bean
+	CorsConfigurationSource corsConfigurationSource()
+	{
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
