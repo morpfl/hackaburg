84a6881e74382827f0ad7366aaa9d8a71f17bd22
Moritz Pflügner <moritz.pfluegner45@gmail.com>
2019-05-25 03:48:25 +0200

    this fucking shit still doesnt work

--- ibot-backend/src/main/java/com/hackathon/ibot/WebSecurity.java
+++ ibot-backend/src/main/java/com/hackathon/ibot/WebSecurity.java
@@ -1,3 +1,5 @@
+package com.hackathon.ibot;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,6 +11,6 @@ public class WebSecurity implements WebMvcConfigurer {
  
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
     }
 }
